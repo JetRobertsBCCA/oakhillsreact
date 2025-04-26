ec8720cca876d6fb2e57a8b4c2cef47295b4f0a9
JetRobertsBCCA <jroberts@basecampcodingacademy.org>
2025-04-26 16:27:32 -0500

    home page

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -2,30 +2,25 @@ import React from 'react';
 import { Link } from '@remix-run/react';
 import Footer from '../../components/Footer/Footer';
 import styles from './route.module.scss';
-import Shotcut000008100Png from '../../../src/assets/shotcut_00_00_08_100.png';
+import Logo1Png from '../../../src/assets/logo1.png';
 
 export default function Home() {
     return (
-        <div>
+        <div className={styles.div2}>
+            <div className={styles.div3}>
+                <img src={Logo1Png} alt="" />
+                <p className={styles.p3}>
+                    Welcome to Oak Hill Stables — your escape to the heart of the countryside.
+                    Tucked away among rolling hills, our ranch is a place where the beauty of nature
+                    and a passion for horses come together. With acres of green pastures, modern
+                    facilities, and a dedicated team who treat every horse like family, Oak Hill
+                    Stables is more than just a destination — it’s an experience. Whether
+                    you&apos;re dreaming of a peaceful retreat, eager to learn the art of
+                    horsemanship, or searching for your perfect equine companion, we can&apos;t wait
+                    to welcome you to the unforgettable world of Oak Hill Stables.
+                </p>
+            </div>
             <div className={styles.pageWrapper}>
-                <div className={styles.heroImage}>
-                    <img src={Shotcut000008100Png} alt="" className={styles.heroImageImg} />
-                    <div className={styles.heroImageContentBox}>
-                        <h1 className={styles.heroImageTitle}>Oak Hill Stable</h1>
-                        <p className={styles.heroImageDescription}>
-                            Welcome to Oak Hill Stables, where the spirit of the countryside meets
-                            the beauty of nature. Nestled in the heart of rolling hills, our ranch
-                            offers a serene haven for horse lovers and nature enthusiasts alike.
-                            With acres of lush pastures, state-of-the-art facilities, and a team
-                            dedicated to the care and training of our magnificent horses, Oak Hill
-                            Stables is more than just a ranch—it's a place where memories are made,
-                            and a love for horses is shared. Whether you're looking for a peaceful
-                            retreat, a place to learn the art of horsemanship, or a new equestrian
-                            companion, we invite you to experience the beauty and tranquility of Oak
-                            Hill Stables.
-                        </p>
-                    </div>
-                </div>
                 <div className={styles.pricing}>
                     <h1 className={styles.pricingTitle}>Our Training Programs</h1>
                     <div className={styles.pricingCards}>
@@ -74,7 +69,9 @@ export default function Home() {
                         </div>
                         <div className={styles.pricingCard}>
                             <div className={styles.pricingPlanOverview}>
-                                <h2 className={styles.pricingHeaderTitle}>Private Birthday Parties</h2>
+                                <h2 className={styles.pricingHeaderTitle}>
+                                    Private Birthday Parties
+                                </h2>
                                 <h1 className={styles.pricingHeaderCost}>$100+</h1>
                                 <p className={styles.pricingHeaderDescription}></p>
                             </div>
